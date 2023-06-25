b0191ec6d2969e73f5b87f2f1e5ec5472fd4f393
theologicalphysicist <adugyamfi3141@gmail.com>
2023-06-25 16:56:58 +0100

    final version (unfinished)

--- README.md
+++ README.md
@@ -3,5 +3,8 @@ Aleph Insights Front-End Developer Technical Task
 
 ### TO RUN:
 
-- install all dependencies: `cd movie_searcher/frontend && npm i`
+- clone the repository
+    - either clone main branch only with `git clone --branch main https://github.com/theologicalphysicist/movie_searcher`
+    - or checkout to main after cloning with `cd movie_searcher/frontend && git checkout main`
+- install all dependencies: `npm i`
 - enter run command: `npm run dev`
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,12 +1,39 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import React, {JSX} from 'react';
 
-const inter = Inter({ subsets: ['latin'] })
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+
+import { Sample } from '@/utils/data';
+
+const inter = Inter({ subsets: ['latin'] });
+
+const TableHeader = (header_string: string): JSX.Element => {
+
+    return (
+        <th>{header_string}</th>
+    );
+};
 
 export default function Home() {
+
+    const HEADERS: string[] = Object.keys(Sample[0]);
+    
+
     return (
         <main className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`} >
-
+            <table>
+                {
+                    HEADERS.map(
+                        (header: string) => {
+                            return (
+                                <React.Fragment key={1}>
+                                    <TableHeader header_string={header} />
+                                </React.Fragment>
+                            );
+                        }
+                    )
+                }
+            </table>
         </main>
     );
 };
